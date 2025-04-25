14638ba8b0d8939f24233456444ab5b3dd5e4c9d
jatharthan <jatharthan94@gmail.com>
2025-04-25 19:34:09 -0400

    networking added

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ module "iam" {
   source         = "./modules/iam"
   project_prefix = var.project_prefix
 }
+
+module "networking" {
+  source         = "./modules/networking"
+  project_prefix = var.project_prefix
+}
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,40 @@ output "iam_role_name" {
   description = "name of the IAM role"
   value       = module.iam.iam_role_name
 }
+
+# networking output
+
+output "vpc_name" {
+  description = "Name of the VPC"
+  value       = module.networking.vpc_name
+}
+
+output "subnet_names" {
+  description = "Names of all subnets"
+  value       = module.networking.subnet_names
+}
+
+output "internal_lb_sg_name" {
+  description = "Security group name for internal load balancer"
+  value       = module.networking.internal_lb_sg_name
+}
+
+output "external_lb_sg_name" {
+  description = "Security group name for external (public) load balancer"
+  value       = module.networking.external_lb_sg_name
+}
+
+output "web_instance_sg_name" {
+  description = "Security group name for public web instances"
+  value       = module.networking.web_instance_sg_name
+}
+
+output "app_instance_sg_name" {
+  description = "Security group name for private app instances"
+  value       = module.networking.app_instance_sg_name
+}
+
+output "db_instance_sg_name" {
+  description = "Security group name for private DB instances"
+  value       = module.networking.db_instance_sg_name
+}
