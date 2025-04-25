94cdf4acb5b3a24cacf6fb7631ec04a9b39f5bb7
jatharthan <jatharthan94@gmail.com>
2025-04-25 19:23:25 -0400

    deleted for testing

--- modules/s3/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "bucket_arn" {
-  description = "ARN of the project S3 bucket"
-  value       = aws_s3_bucket.project_bucket.arn
-}
--- modules/s3/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "project_prefix" {
-  description = "Project prefix to be used in naming the bucket"
-  type        = string
-}
