a586d55e66978aa2cbac6e70c24ba2136f4e50e8
Juhi Choudhary <115893427+Juhi5863@users.noreply.github.com>
2025-06-15 13:52:54 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "juhi-tf-state-dev"
+    bucket         = "juhi-tf-state-dev-pipeline"
     key            = "global/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "juhi-terraform-lock-table-dev"
